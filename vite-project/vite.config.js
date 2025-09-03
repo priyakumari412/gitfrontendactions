d78652aa79fd32a26a13ac517d05af9baf677c16
Priya Kumari <priya.kumari4122004@gmail.com>
2025-09-03 18:17:57 +0530

    my final commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/gitfrontendactions/',
   plugins: [react()],
-  test:{
-    environment:'jsdom',
-    globals:true,
-    setupFiles:'./src/setup.js'
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setup.js'
 
   }
 })
